7fc4dea31a7c3f876b1be0b591b18e2c5164c0e2
JoaoMelo2026 <absolgxmega@gmail.com>
2026-09-05 07:26:55 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,71 @@
-function greet(){
-const username = window.prompt('Digite seu nome: ');
+let repeat = true;
 
-if (!username){
-    if (username !== '')
-        return window.alert('Operação cancelada.');
+function greet() {
+  const username = window.prompt('Digite seu nome:');
 
-    return window.alert(`Olá! Seja bem-vindo!`);
-}
+  if (!username) {
+    if (username !== '') {
+      repeat = false;
+      return window.alert('Operação cancelada.');
+    }
 
-window.alert(`Olá, ${username}! Seja vem vindo.`);
+    return window.alert(`Olá! Seja bem vindo.`);
+  }
+
+  window.alert(`Olá, ${username}! Seja bem vindo.`);
 }
 
 greet();
+
+let balance = 0;
+
+const operations = {
+  check: () => window.alert(`Saldo atual: R$ ${balance}`),
+  deposit: () => {
+    const value = Number(window.prompt('Valor do depósito:'));
+
+    balance += value;
+
+    window.alert(`Saldo atual: R$ ${balance}`);
+  },
+  cashout: () => {
+    const value = Number(window.prompt('Valor do saque:'));
+
+    balance -= value;
+
+    window.alert(`Saldo atual: R$ ${balance}`);
+  },
+  exit: () => {
+    window.alert('Foi um prazer atendê-lo.');
+    repeat = false;
+  },
+  invalid: () => window.alert('Número inválido.')
+}
+
+while (repeat) {
+  const operation = window.prompt(`
+    Escolha uma operação:
+  
+    1 - Consultar
+    2 - Depositar
+    3 - Sacar
+    4 - Sair
+  `);
+
+  if (operation === null) {
+    window.alert('Operaçao cancelada.');
+    repeat = false;
+  } else if (operation === '') {
+    window.alert('Digite um número válido.');
+  } else if (isNaN(operation)) {
+    window.alert('Digite um número válido.');
+  } else {
+  switch (Number(operation)) {
+    case 1: operations.check(); break;
+    case 2: operations.deposit(); break;
+    case 3: operations.cashout(); break;
+    case 4: operations.exit(); break;
+    default: operations.invalid();
+  }
+}
+}
