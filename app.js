980e0b737ce4c40a3a5575e7d89c0bcbb075f96f
JoaoMelo2026 <absolgxmega@gmail.com>
2026-09-05 15:05:16 -0300

    Update app.js

--- app.js
+++ app.js
@@ -18,49 +18,115 @@ function greet() {
 greet();
 
 let balance = 0;
+const statement = [];
 
 const operations = {
-  check: () => window.alert(`Saldo atual: R$${balance}`),
-  deposit: () => {const value = Number(window.prompt('Valor do depósito?'));
-  balance += value;
+  check: () => window.alert(`Saldo atual: R$ ${balance}`),
+  deposit: () => {
+    const response = window.prompt('Valor do depósito:');
 
-  window.alert(`Saldo atual: R$${balance}`);},
+    if (response === null) return window.alert('Operação cancelada.');
 
-  cashout: () => {const value = Number(window.prompt('Valor do saque?'));
-  balance -= value;
+    const value = Number(response);
 
-  window.alert(`Saldo atual: R$${balance}`);},
+    if (value < 1) {
+      window.alert('Digite um valor válido.');
+      return operations.deposit();
+    } else if (isNaN(value)) {
+      window.alert('Digite um valor válido.');
+      return operations.deposit();
+    }
 
-  exit: () => {window.alert('Foi um prazer atendê-lo!');
-  repeat = false;},
+    balance += value;
 
+    statement[statement.length] = {
+      type: 'Depósito',
+      value: value
+    };
+
+    window.alert(`Saldo atual: R$ ${balance}`);
+  },
+  cashout: () => {
+    const response = window.prompt('Valor do saque:');
+
+    if (response === null) return window.alert('Operação cancelada.');
+
+    const value = Number(response);
+
+    if (value < 1) {
+      window.alert('Digite um valor válido.');
+      return operations.cashout();
+    } else if (isNaN(value)) {
+      window.alert('Digite um valor válido.');
+      return operations.cashout();
+    } else if (value > balance) {
+      window.alert('Saldo indisponível.');
+      return operations.cashout();
+    }
+
+    balance -= value;
+
+    statement[statement.length] = {
+      type: 'Saque',
+      value: value
+    };
+
+    window.alert(`Saldo atual: R$ ${balance}`);
+  },
+  statement: () => {
+    if (statement.length === 0) {
+      return window.alert('Nenhuma transação realizada.');
+    }
+
+    let text = 'Extrato:\n\n';
+
+    for (let i = 0; i < statement.length; i++) {
+      text += `${statement[i].type} = R$ ${statement[i].value}\n`;
+    }
+
+    window.alert(text);
+  },
+  exit: () => {
+    const response = window.confirm('Deseja realmente sair?');
+
+    if (!response) return;
+
+    window.alert('Foi um prazer atendê-lo.');
+    repeat = false;
+  },
   invalid: () => window.alert('Número inválido.')
 }
 
-while(repeat){
-const operation = window.prompt(`
- Escolha uma operação:
- 
- 1- Consultar
- 2- Depositar
- 3- Sacar
- 4- Sair
-`);
+while (repeat) {
+  const operation = window.prompt(`
+    Escolha uma operação:
+  
+    1 - Consultar
+    2 - Depositar
+    3 - Sacar
+    4 - Extrato
+    5 - Sair
+  `);
 
-if (operation === null) {
-  window.alert('Operação cancelada.');
-  repeat = false;
-} else if (operation === '') {
- window.alert('Digite um número.')
-} else if (isNaN(operation)) {
-  window.alert('Digite um número válido.');
-} else{
-switch (Number(operation)) {
-  case 1: operations.check();break;
-  case 2: operations.deposit();break;
-  case 3: operations.cashout();break;
-  case 4: operations.exit();break;
-  default: operations.invalid();
+  if (operation === null) {
+    const response = window.confirm('Deseja realmente cancelar?');
+
+    if (!response) continue;
+
+    window.alert('Operaçao cancelada.');
+    repeat = false;
+  } else if (operation === '') {
+    window.alert('Digite um número válido.');
+  } else if (isNaN(operation)) {
+    window.alert('Digite um número válido.');
+  } else {
+    switch (Number(operation)) {
+      case 1: operations.check(); break;
+      case 2: operations.deposit(); break;
+      case 3: operations.cashout(); break;
+      case 4: operations.statement(); break;
+      case 5: operations.exit(); break;
+      default: operations.invalid();
+    }
   }
-}
 }
